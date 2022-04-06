79255c25f1ad292f9d20a6d357cbdce7aa3071b2
Vitor Gonçalves Vieira <vitorgoncalvesvieira22@gmail.com>
2022-04-06 19:54:37 -0300

    Ajustes e implementacao de botoes na interface grafica realizado.

--- interfaceGrafica/primeira_tela.py
+++ interfaceGrafica/primeira_tela.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from turtle import width
+from turtle import width 
 
 root = Tk()
 
@@ -8,11 +8,26 @@ class Aplicacao():
         self._root = root
         self.configuracoes_abertura()
         self.tela_inicial_qr()
+        self.widgets_frame1()
         root.mainloop()
+
     def configuracoes_abertura(self):
-        self._root.title("Texto barra Superior") #texto barra superior
+        self._root.title("Gerador de QR code") #texto barra superior
         self._root.configure(background="#6959CD")#cor background
-        self._root.geometry("800x680") #tamanho tela
+
+        #=============abrir centralizado=============
+        w = 800
+        h = 600
+
+        ws = root.winfo_screenwidth()
+        hs = root.winfo_screenheight()
+        x = (ws/2) - (w/2)
+        y = (hs/2) - (h/2)
+
+        root.geometry('%dx%d+%d+%d' % (w, h, x, y))
+        #============================================
+
+        # self._root.geometry(f'{w}x{h}') #tamanho tela
         self._root.resizable(True, True) #responsivel vertical e horizontal
         self._root.maxsize(width=1920, height=1080) #maximo tela
         self._root.minsize(width=480, height=350) #minimo tela
@@ -20,10 +35,15 @@ class Aplicacao():
     def tela_inicial_qr(self):
         self._frame_1 = Frame(self._root, bd=4, bg="#1E90FF", highlightbackground="#99208a", 
         highlightthickness=2)
-        self._frame_1.place(relx=0.02, rely=0.02, relwidth=0.96, relheight=0.46)
+        self._frame_1.place(relx=0.01, rely=0.01, relwidth=0.98, relheight=0.98)
 
-        self._frame_2 = Frame(self._root, bd=4, bg="#1E90FF", highlightbackground="#99208a", 
-        highlightthickness=2)
-        self._frame_2.place(relx=0.02, rely=0.5, relwidth=0.96, relheight=0.45)
+    def widgets_frame1(self):
+        self._botao_limpar = Button(self._frame_1, text="Limpar", font=("Helvetica", 14))
+        self._botao_limpar.place(relx=0.523, rely=0.95, relwidth=0.12, relheight=0.06 , anchor="w")
 
+        self._botao_limpar = Button(self._frame_1, text="Gerar QR", font=("Helvetica", 14))
+        self._botao_limpar.place(relx=0.5, rely=0.95, relwidth=0.12, relheight=0.06 , anchor="e")
+
+    
+ 
 Aplicacao(root)
